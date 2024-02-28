6dbf902e8bcb1ea4946f28cfd91aa7c4e2891e55
Aditya Agarwal <adityagarwal05@gmail.com>
2024-02-28 11:46:08 -0500

    User input file for downloading

--- CodingWebsite/src/Main.java
+++ CodingWebsite/src/Main.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Learning Object Oriented Programming!");
-        System.out.println("test");   
+        Scanner input = new Scanner(System.in);
+        System.out.println("In this tutorial, we are gaining User input to print out their name with a have a good day message. What is your name?");
+        String UserName = input.nextLine();
+        System.out.println("Hello " + UserName + ", Have a good day!");
+        input.close();
     }
 }
