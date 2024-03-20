314a857310e33b323447021ddaca1b7f8bc886c6
Aditya Agarwal <adityagarwal05@gmail.com>
2024-03-20 16:16:50 -0400

    Added First tutorial Link

--- CodingWebsite/src/firstTutorial.java
+++ CodingWebsite/src/firstTutorial.java
@@ -4,9 +4,9 @@ public class firstTutorial {
     public static void main(String[] args) {
         System.out.println("Welcome to Learning Object Oriented Programming!");
         Scanner input = new Scanner(System.in);
-        System.out.println("In this tutorial, we are gaining User input to print out their name with a have a good day message. What is your name?");
+        System.out.println("In this tutorial, we are gaining User input to print out their name with a sweet message. What is your name?");
         String UserName = input.nextLine();
-        System.out.println("Hello " + UserName + ", Have a good day!");
+        System.out.println("Hello " + UserName + ", You are the coolest person!");
         input.close();
     }
 }
